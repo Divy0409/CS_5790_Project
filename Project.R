beedf8e125102cefc1a48f08460e2f391511571d
huntermali <hemalino@mtu.edu>
2024-09-25 13:22:48 -0400

    Update Project.R

--- Project.R
+++ Project.R
@@ -7,3 +7,13 @@ install.packages(c("caret", "corrplot", "e1071", "lattice", "mlbench"))
 # Load data
 diabetes = read.csv("diabetic_data.csv")
 str(diabetes)
+
+# Missing values
+image(is.na(diabetes), main="Missing Values", xlab="Observation", ylab="Variable", xaxt="n", yaxt="n", bty="n", col=topo.colors(6))
+axis(1,seq(0,1,length.out = nrow(diabetes)), 1:nrow(diabetes), col="white")
+
+install.packages(c("naniar", "tidyverse"))
+library(naniar)
+print(n=100, miss_case_summary(diabetes))
+miss_var_summary(diabetes)
+
