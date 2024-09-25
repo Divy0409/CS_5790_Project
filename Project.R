949a56d56fcb736df526ce192429eeb1566c844b
huntermali <hemalino@mtu.edu>
2024-09-25 13:48:26 -0400

    Update Project.R

--- Project.R
+++ Project.R
@@ -9,6 +9,7 @@ diabetes = read.csv("diabetic_data.csv")
 str(diabetes)
 
 # Missing values
+
 image(is.na(diabetes), main="Missing Values", xlab="Observation", ylab="Variable", xaxt="n", yaxt="n", bty="n", col=topo.colors(6))
 axis(1,seq(0,1,length.out = nrow(diabetes)), 1:nrow(diabetes), col="white")
 
@@ -16,4 +17,12 @@ install.packages(c("naniar", "tidyverse"))
 library(naniar)
 print(n=100, miss_case_summary(diabetes))
 miss_var_summary(diabetes)
+n_miss(diabetes)
+
+colNames <- colnames(diabetes)
+print(colNames)
+for(col in colNames) {
+  print(col)
+  print(var(diabetes[, col]), na.rm=TRUE)
+}
 
